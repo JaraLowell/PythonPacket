08c99bb650cf04b67c787aa5c42e80087b3de778
Jara <jara@mivabe.nl>
2024-06-03 14:12:49 +0200

    Update ForMichel.py
    Add color~

--- ForMichel.py
+++ ForMichel.py
@@ -22,7 +22,7 @@ async def process_request(sever_root, path, request_headers):
     response_headers = [('Server', 'asyncio websocket server'), ('Connection', 'close')]
     full_path = os.path.realpath(os.path.join(sever_root, path[1:]))
     if os.path.commonpath((sever_root, full_path)) != sever_root or not os.path.exists(full_path) or not os.path.isfile(full_path):
-        print("HTTP GET {} 404 NOT FOUND".format(path))
+        print("[" + time.strftime("%H:%M:%S", time.gmtime()) + "] [Network] HTTP GET {} 404 File not found".format(path))
         return HTTPStatus.NOT_FOUND, [], b'404 NOT FOUND'
 
     extension = full_path.split(".")[-1]
@@ -33,18 +33,18 @@ async def process_request(sever_root, path, request_headers):
     return HTTPStatus.OK, response_headers, body
 
 async def register(websocket):
-    print("[NETWORK] New WebSocket connection from", str(websocket.remote_address)[1:-1])
+    print("[" + time.strftime("%H:%M:%S", time.gmtime()) + "] [Network] New WebSocket connection from", str(websocket.remote_address)[1:-1])
     USERS.add(websocket)
 
 async def unregister(websocket):
-    print("[NETWORK] WebSocket connection closed for", str(websocket.remote_address)[1:-1])
+    print("[" + time.strftime("%H:%M:%S", time.gmtime()) + "] [Network] WebSocket connection closed for", str(websocket.remote_address)[1:-1])
     USERS.remove(websocket)
 
 async def mysocket(websocket, path):
     await register(websocket)
     try:
         async for message in websocket:
-            print("[NETWORK] " + message)
+            print("[" + time.strftime("%H:%M:%S", time.gmtime()) + "] [Network] " + message)
             await sendmsg(message)
     finally:
         await unregister(websocket)
@@ -66,13 +66,24 @@ async def main():
     while True:
         line = await stdin_reader.readline()
         await sendmsg(line.decode())
-        print("\033[F[CONSOLE] " + line.decode() + "\033[A")
+        print("\033[F[" + time.strftime("%H:%M:%S", time.gmtime()) + "] [Console] " + line.decode() + "\033[A")
 
 if __name__ == "__main__":
     os.system("") # in case the above \033[F no works like windows...
+
+    print("\u001B[8;37;44m");
+    print("  _   _           _     ______          _        _      ")
+    print(" | \\ | |         | |    | ___ \\        | |      | |     ")
+    print(" |  \\| | ___   __| | ___| |_/ /_ _  ___| | _____| |_    ")
+    print(" | . ` |/ _ \\ / _` |/ _ \\  __/ _` |/ __| |/ / _ \\ __|   ")
+    print(" | |\\  | (_) | (_| |  __/ | | (_| | (__|   <  __/ |_    ")
+    print(" \\_| \\_/\\___/ \\__,_|\\___\\_|  \\__,_|\\___|_|\\_\\___|\\__|   ")
+    print("\u001B[32m  An open source Python WS Packet server V1.1ß          ")
+    print("\u001B[32m  For WA8DED Modems that support HostMode               \u001B[0m")
+
     handler = functools.partial(process_request, os.getcwd() + '/www')
     start_server = websockets.serve(mysocket, '0.0.0.0', MYPORT, process_request=handler)
-    print("[NETWORK] Running server at http://localhost:%d/" % MYPORT)
+    print("[" + time.strftime("%H:%M:%S", time.gmtime()) + "] [Network] \u001B[33mRunning server at http://localhost:%d/\u001B[0m" % MYPORT)
 
     loop = asyncio.get_event_loop()
     try:
