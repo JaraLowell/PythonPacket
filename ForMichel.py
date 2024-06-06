010e9545e061c1c92c55d839fa08ec07c7581bac
TronicSoftware <159629135+TronicSoftware@users.noreply.github.com>
2024-06-06 22:50:34 +0200

    add logheard fuction

--- ForMichel.py
+++ ForMichel.py
@@ -266,6 +266,21 @@ def init_tncConfig():
     ser.write(beacon)
     ser.readline()
 
+def logheard(callsign, status, locator):
+    timenow = int(time.time())
+    print(timenow)
+    print(callsign)
+    print(status)
+    print(locator)
+    if callsign in MHeard:
+        MHeard[callsign][1] = locator
+        MHeard[callsign][3] = timenow
+        MHeard[callsign][4] += 1
+    else:
+        MHeard[callsign] = ['', locator, timenow, timenow, 1, 0, 0, 0]
+
+    print(MHeard)
+
 async def go_serial():
     # serial port stuff here
     global polling
